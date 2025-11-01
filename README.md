a625c999d48cac1dd094bcf4949147024adbca9d
mrsan <mrsandv@pm.me>
2025-11-01 10:44:12 -0600

    Priori-taks example

--- README.md
+++ README.md
@@ -1,36 +1,75 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## 🎯 Prioritask - To-Do App to Rank Tasks
 
-## Getting Started
+A simple yet effective To-Do list app built with Next.js and Tailwind CSS.
 
-First, run the development server:
+The twist in this project is priority system that automatically sorts your pending taks to help you focus on what matter most.
+
+This projects was a short development simulating the environment of an interview to demostrate the quick implementation of key React and Typescript concepts.
+
+[Demo](https://todo.spacehole.tech)
+
+![Priori-task screen](https://x27ogsxot4y4om7r.public.blob.vercel-storage.com/todo.png)
+
+## ✨ Features
+
+- Task Management: Create, complete, and delete tasks.
+
+- Priority System: Assign one of four priority levels (Urgen, important, next week, Not important).
+
+- Automatic Sorting: All the list "Pending" and "Completed" are sorted by priority, showing the most urgent tasks first.
+
+- Conditional Styling: Each task has a different border color depending on its priority, making it easy to identify visually.
+
+- Data Persistence: Tasks are automatically saved in the browser's `localStorage`, so ther're not lost when you reload the page.
+
+## 🛠️ Tech Stack
+
+This project was built using:
+
+- Next.js (App Router) - Framework for React.
+
+- React - For building the user interface.
+
+- TypeScript - For string typing and safer code.
+
+- Tailwind CSS - For fast, modern styling.
+
+- React Hooks - (`useState` and `useEffect`) for state management and persistence.
+
+## 💡 Key learnings
+
+Beyond a simple To-Do lists, this project was a good exercise to practice:
+
+- Immutable State Management: Using `.map` and `.filter()` to update the tasks array without mutating it.
+
+- Effect Hooks (`useEffect`): Implementing two separate `useState` hooks:
+
+  - One to load the tasks to `localStorage` only once (with `[]` as a dependency).
+
+  - Another to save tasks to `localStorage` every time the `tasks` state changes (with `[tasks]` as a dependency).
+
+- Type-Safety Forms: Handling `onChange` events for multiple input types (text and select) while ensuring data consistency.
+
+- Conditional and Dynamic Rendering: Using `priorityMap` to apply tailwind styles dynamically.
+
+## 🚀 Getting Started
+
+This is a Next.js project.
+
+First, install the dependencies:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+pnpm i
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+o use yarn, npm... your choice.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+Then, run the development server:
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+Bash
 
-## Learn More
+```bash
+pnpm run dev
+```
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+Open http://localhost:3000 in your browser to see the result.
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,188 @@
-import Image from "next/image";
+"use client";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+type TTask = {
+  id: string;
+  text: string;
+  priority: 0 | 1 | 2 | 3;
+  completed: boolean;
+};
+
+const priorityMap = {
+  0: { copy: "Urgent", styles: "border-rose-500" },
+  1: { copy: "Important", styles: "border-orange-500" },
+  2: { copy: "Next week", styles: "border-green-500" },
+  3: { copy: "Not important", styles: "border-indigo-500" },
+};
+
+type TTaskElement = TTask & {
+  onCheck: (id: string) => void;
+  onRemove: (id: string) => void;
+};
+
+const TaskElement = ({
+  text,
+  priority,
+  completed,
+  id,
+  onCheck,
+  onRemove,
+}: TTaskElement) => (
+  <li
+    className={`flex h-10 bg-zinc-900 text-zinc-200 justify-between items-center p-2 ${priorityMap[priority].styles} border rounded-lg`}
+  >
+    <div className={`flex items-center ${completed ? "line-through":"" }`}>
+      <input
+        className="mx-2"
+        type="checkbox"
+        checked={completed}
+        onChange={() => {
+          onCheck(id);
+        }}
+      />
+      {text}
+    </div>
+    <button
+      className="cursor-pointer hover:opacity-70"
+      onClick={() => {
+        onRemove(id);
+      }}
+    >
+      ❌
+    </button>
+  </li>
+);
+
+const createId = () => {
+  return (Math.random() + 1).toString(36).substring(2, 10);
+};
 
 export default function Home() {
+  const defaultTask: TTask = {
+    text: "",
+    id: "",
+    priority: 3,
+    completed: false,
+  };
+  const [task, setTask] = useState<TTask>(defaultTask);
+  const [tasks, setTasks] = useState<TTask[]>([]);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setTask((prevTask) => ({
+      ...prevTask,
+      [name]: name === "priority" ? Number(value) : value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const id = createId();
+    setTasks((prevTasks) => [...prevTasks, { ...task, id }]);
+    setTask(defaultTask);
+  };
+
+  const handleCheck = (id: string) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => {
+        return task.id === id ? { ...task, completed: !task.completed } : task;
+      })
+    );
+  };
+
+  const handleRemove = (id: string) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+  };
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const taskCount = tasks.filter((t=>!t.completed)).length;
+  const sortedTasks = [...tasks].sort((a,b)=> a.priority - b.priority)
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex h-screen w-screen md:h-[70vh] md:w-[60vw] flex-col items-center justify-center p-5 bg-zinc-900 shadow shadow-rose-500/10 rounded-lg">
+      <form
+        className="bg-zinc-800 flex h-18 w-full md:w-1/2 text-black rounded-sm items-center justify-between p-3 gap-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          required
+          className="w-full h-10 bg-zinc-50 p-2 rounded-sm text-zinc-800"
+          type="text"
+          value={task.text}
+          onChange={handleChange}
+          name="text"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        <select
+          name="priority"
+          onChange={handleChange}
+          value={task.priority}
+          className="p-2 text-zinc-200"
+        >
+          {Object.entries(priorityMap).map(([key, value]) => (
+            <option key={key} value={Number(key)}>
+              {value.copy}
+            </option>
+          ))}
+        </select>
+        <button
+          type="submit"
+          className="bg-rose-800 rounded-lg px-5 py-2 text-zinc-100 hover:opacity-70 cursor-pointer"
+        >
+          Add
+        </button>
+      </form>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-2 p-2">
+        <div className="h-[50vh] overflow-scroll">
+          <h1 className="text-zinc-200 text-xl p-3 mx-auto w-full text-center my-2 uppercase">
+            Pending
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <ul className="flex flex-col gap-2 overflow-scroll px-5">
+            {sortedTasks
+              .filter((item) => !item.completed)
+              .map((it) => (
+                <TaskElement
+                  key={it.id}
+                  {...it}
+                  onCheck={handleCheck}
+                  onRemove={handleRemove}
+                />
+              ))}
+          </ul>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div>
+          <h1 className="text-zinc-200 text-xl p-3 mx-auto w-full text-center my-2 uppercase">
+            Completed
+          </h1>
+          <ul className="flex flex-col gap-2 overflow-scroll px-5">
+            {sortedTasks
+              .filter((item) => item.completed)
+              .map((it) => (
+                <TaskElement
+                  key={it.id}
+                  {...it}
+                  onCheck={handleCheck}
+                  onRemove={handleRemove}
+                />
+              ))}
+          </ul>
         </div>
-      </main>
-    </div>
+      </div>
+
+      <p className="text-zinc-200">
+        You have {taskCount} pending tasks
+      </p>
+    </main>
   );
 }
